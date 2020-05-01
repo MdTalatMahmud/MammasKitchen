cdb2bd6fceb10be0b518ba705c9ac89de6aedcbe
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-05-01 17:39:58 +0600

    modify

--- resources/views/admin/Slider/index.blade.php
+++ resources/views/admin/Slider/index.blade.php
@@ -3,8 +3,8 @@
 @section('title', 'Slider')
 
 @push('css')
-{{--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">--}}
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap.min.css">
+
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css">
     @endpush
 
 @section('content')
@@ -65,18 +65,21 @@
 
     @endsection
 
+        @push('scripts')
 
 
-    @push('scripts')
-{{--            <script src="https://code.jquery.com/jquery-3.3.1.js"></script>--}}
             <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-            <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap.min.js"></script>
+            <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
+
             <script>
                 $(document).ready(function() {
-                    $('#example').DataTable();
+                    $('#table').DataTable();
                 } );
             </script>
+
         @endpush
+
+
 
 
 
