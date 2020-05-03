8928fb5349a85a37fcb738224812ae66b99b5cbf
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-05-03 17:06:03 +0600

    comment

--- resources/views/admin/Slider/index.blade.php
+++ resources/views/admin/Slider/index.blade.php
@@ -10,7 +10,7 @@
 @section('content')
     <div class="content">
 
-
+{{--        message showing--}}
         @if(session('successMsg'))
             <div class="alert alert-dismissible alert-success">
                 <button type="button" class="close" data-dismiss="alert">x</button>
