aa31d8629770ec47c7bf2577c386a79ec8efed9c
Muhammad Talat Mahmud <36367916+MdTalatMahmud@users.noreply.github.com>
2020-06-21 21:30:04 +0600

    -fetch image from database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Slider;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $sliders = Slider::all();
+        return view('welcome', compact('sliders'));
     }
 }
